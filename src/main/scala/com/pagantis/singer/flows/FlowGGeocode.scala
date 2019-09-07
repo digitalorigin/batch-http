549cc8fa07cbe2b556d134245dd4cb9adf2eafb1
dcereijodo <dcereijo@digitalorigin.com>
2019-09-07 19:15:28 +0200

    refactor and simiplify code

--- src/main/scala/com/pagantis/singer/flows/FlowGGeocode.scala
+++ src/main/scala/com/pagantis/singer/flows/FlowGGeocode.scala
@@ -1,59 +1,75 @@
 package com.pagantis.singer.flows
 
-import java.time.ZoneId
-
 import akka.actor.ActorSystem
 import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Flow, StreamConverters}
+import akka.http.scaladsl.model.HttpResponse
+import akka.stream.{ActorMaterializer, Materializer}
+import akka.stream.scaladsl.StreamConverters
+import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContextExecutor, Future}
-import scala.util.Success
+import scala.util.{Failure, Success, Try}
 
 object FlowGGeocode extends App {
 
   val clazz = getClass.getName
 
   // init actor system, loggers and execution context
-  implicit val system: ActorSystem = ActorSystem("GGeocodeFlow")
+  implicit val system: ActorSystem = ActorSystem("Geocode")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val standardLogger: LoggingAdapter = Logging(system, clazz)
   implicit val ec: ExecutionContextExecutor = system.dispatcher
 
-  val singerAdapter = SingerAdapter.fromConfig
-  val ggcodeFlow = GGeocode.fromConfig
+  val config = ConfigFactory.load()
+  val endpoint = config.getString("flow.endpoint")
 
-  val timeExtracted = java.time.LocalDateTime.now().atZone(ZoneId.of("UTC"))
+  val startTime = System.nanoTime()
 
-  StreamConverters
-    .fromInputStream(() => System.in)
-    .log(clazz)
-    .mapConcat(_.utf8String.split("\n").toList)
-    .log(clazz)
-    .map(singerAdapter.parseRecord(_))
-    .log(clazz)
-    .via(ggcodeFlow)
-    .log(clazz)
-    .map{case (response, address) => singerAdapter.toSingerRecord(response, address, timeExtracted)}
-    .log(clazz)
-    .map(singerAdapter.toJsonString(_))
-    .runForeach(println(_))
-    .onComplete(res => {
-        Http().shutdownAllConnectionPools.andThen { case _ =>
-          materializer.shutdown
-          system.terminate
-        }
-        res match {
-          case Success(_) => sys.exit(0)
-          case _ => sys.exit(1)
-        }
+  def parseResponse(triedResponse: (Try[HttpResponse], Address))(implicit am: Materializer) =
+    triedResponse match {
+      case (Success(response), address) => Geocode.fromHttpResponse(response)
+      case (Failure(exception), address) => {
+        throw exception
       }
-    )
+    }
 
-  // block main thread forever
-  // exit will be handled on the stream exit callback
-  Await.ready(Future.never, Duration.Inf)
+  // This shutdown sequence was copied from another related issue: https://github.com/akka/akka-http/issues/907#issuecomment-345288919
+  def shutdownSequence =
+    Http().shutdownAllConnectionPools
+
+  val flowComputation =
+    StreamConverters
+      .fromInputStream(() => System.in)
+      .log(clazz)
+      .mapConcat(_.utf8String.split("\n").toList)
+      .log(clazz)
+      .map(SingerMessage.fromLine)
+      .log(clazz)
+      .map(
+        singerMessage => {
+          val address = Address.fromSingerMessage(singerMessage)
+          (address.toHttpRequest, address)
+        }
+      )
+      .log(clazz)
+      .log(clazz)
+      .via(Http().cachedHostConnectionPoolHttps[Address](host = endpoint))
+      .log(clazz)
+      .map(parseResponse(_))
+      .log(clazz)
+      .runForeach(println(_))
+
+  Await.ready(flowComputation, Duration.Inf)
+
+  standardLogger.info(s"Total execution time: ${(System.nanoTime - startTime)/1000000000} seconds")
+
+  Await.ready(shutdownSequence, Duration.Inf)
+
+  flowComputation.value match {
+    case Some(Success(_)) => sys.exit(0)
+    case _ => sys.exit(1)
+  }
 
 }
---
+++ src/main/scala/com/pagantis/singer/flows/SingerMessage.scala
@@ -0,0 +1,42 @@
+package com.pagantis.singer.flows
+
+import spray.json._
+import spray.json.JsValue
+
+case class SingerMessage(`type`: String = "RECORD", stream: String = "geocode", record: JsValue)
+
+object JsonProtocol extends DefaultJsonProtocol {
+
+  implicit object SingerMessagFormat extends RootJsonFormat[SingerMessage] {
+
+    def write(message: SingerMessage) =
+      JsObject(
+        "type" -> JsString(message.`type`),
+        "stream" -> JsString(message.stream),
+        "record" -> message.record
+      )
+
+    def read(value: JsValue) = value.asJsObject.getFields("type", "stream", "record") match {
+      case  Seq(JsString("RECORD"), JsString("geocode"), record) =>
+        SingerMessage(record = record)
+      case _ => deserializationError("Singer.io message expected")
+    }
+
+  }
+
+  implicit val addressFormat = jsonFormat4(Address.apply)
+
+}
+
+
+import JsonProtocol._
+
+object SingerMessage {
+
+  def fromLine(line: String): SingerMessage = line.parseJson.convertTo[SingerMessage]
+
+  def toLine(message: SingerMessage): String = message.toJson.compactPrint
+
+}
+
+
